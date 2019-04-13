ec55a5bf4087e3f08acb600a5eea84f2c3607704
Django Cass <dj.cass44@gmail.com>
2019-04-13 12:38:40 +1000

    Fixed an issue where non-coloured logs weren't printed Added additional testing Added missing copyright headers Added build status

--- src/main/kotlin/dev/castive/log2/Log.kt
+++ src/main/kotlin/dev/castive/log2/Log.kt
@@ -1,3 +1,20 @@
+/*
+ *    Copyright 2019 Django Cass
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
 package dev.castive.log2
 
 import java.text.SimpleDateFormat
@@ -53,16 +70,14 @@ object Log {
 
     private fun log(name: String, msg: String, priority: Int, colour: String? = null) {
         if(priorityLevel > priority) return
-        if(colour == null) {
-            "${timeFormat.format(System.currentTimeMillis())} | [${Thread.currentThread().name}] |-${priorities[priority]} in $name - $msg"
-            return
-        }
+        println(getMessage(name, msg, priority, colour))
+    }
+
+    internal fun getMessage(name: String, msg: String, priority: Int, colour: String? = null): String {
+        if(colour == null) return "${timeFormat.format(System.currentTimeMillis())} | [${Thread.currentThread().name}] |-${priorities[priority]} in $name - $msg"
         // Display with pretty colours
-        println(if(USE_SHORT_COLOURS) {
-            "${timeFormat.format(System.currentTimeMillis())} | [${Thread.currentThread().name}] |-$colour${priorities[priority]}$ANSI_RESET in $name - $msg"
-        } else {
-            "$colour${timeFormat.format(System.currentTimeMillis())} | [${Thread.currentThread().name}] |-${priorities[priority]} in $name - $msg$ANSI_RESET"
-        })
+        return if(USE_SHORT_COLOURS) "${timeFormat.format(System.currentTimeMillis())} | [${Thread.currentThread().name}] |-$colour${priorities[priority]}$ANSI_RESET in $name - $msg"
+        else "$colour${timeFormat.format(System.currentTimeMillis())} | [${Thread.currentThread().name}] |-${priorities[priority]} in $name - $msg$ANSI_RESET"
     }
 
     public fun setPriorityLevel(level: Int) {
--- src/test/kotlin/dev/castive/log2/LogTest.kt
+++ src/test/kotlin/dev/castive/log2/LogTest.kt
@@ -1,11 +1,35 @@
+/*
+ *    Copyright 2019 Django Cass
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
 package dev.castive.log2
 
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
+import java.text.SimpleDateFormat
 
 class LogTest {
+    @BeforeEach
+    internal fun setUp() {
+        Log.setPriorityLevel(0)
+    }
+
     @Test
     fun manualColours() {
         Log.USE_SHORT_COLOURS = true
@@ -30,6 +54,29 @@ class LogTest {
     }
     @ParameterizedTest
     @ValueSource(strings = [
+        Log.ANSI_GREEN,
+        Log.ANSI_RED,
+        Log.ANSI_WHITE_BACKGROUND,
+        Log.ANSI_YELLOW
+    ])
+    fun testColourChange(colour: String) {
+        Log.USE_SHORT_COLOURS = false
+        val log = Log.getMessage(javaClass.name, "Test message", 0, colour)
+        println(log)
+        assert(log.startsWith(colour))
+        assert(log.endsWith(Log.ANSI_RESET))
+    }
+    @Test
+    fun testNullColour() {
+        Log.USE_SHORT_COLOURS = false
+        val log = Log.getMessage(javaClass.name, "Test message", 0, null)
+        println(log)
+        // Non-coloured log should start with the year
+        assertTrue(log.startsWith(SimpleDateFormat("yyyy").format(System.currentTimeMillis())))
+        assertFalse(log.endsWith(Log.ANSI_RESET))
+    }
+    @ParameterizedTest
+    @ValueSource(strings = [
         "INFO",
         "VERBOSE",
         "ERROR",
@@ -42,6 +89,6 @@ class LogTest {
     ])
     fun testNamedSetter(name: String) {
         Log.setPriority(name)
-        Assertions.assertEquals(name, Log.getPriority())
+        assertEquals(name, Log.getPriority())
     }
 }
